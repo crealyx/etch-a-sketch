a9277c61f81222329aae8e1fbed33ad323789333
crealyx <sanberk5416@hotmail.com>
2021-04-20 14:51:16 +0300

    Add 'clear' button to change canvas color back to default color

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 const gridContainer = document.querySelector('#grid-container');
+const clear = document.querySelector('#clear');
+const color = document.querySelector('#color');
 
 let grids = [];
 
 for(i = 0; i < 256; i++){
     grids = document.createElement('div');
+    grids.classList.add('canvas');
     gridContainer.append(grids);
 };
 
@@ -11,6 +14,13 @@ gridContainer.addEventListener('mouseover', e => {
     if(e.target.id !== 'grid-container'){
         e.target.style.backgroundColor = 'red';
     }
-    
 });
 
+const canvas = Array.from(document.querySelectorAll('.canvas'));
+
+clear.addEventListener('click', () => {
+    canvas.forEach(e => {
+        e.style.backgroundColor = 'rgb(212, 212, 212)';
+    });
+    
+})
