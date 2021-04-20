148c01467221e1d159b9d80c0a7614065d56b5ba
crealyx <sanberk5416@hotmail.com>
2021-04-20 18:33:11 +0300

    Add color picker

--- script.js
+++ script.js
@@ -1,13 +1,29 @@
 const gridContainer = document.querySelector('#grid-container');
 const clear = document.querySelector('#clear');
 const color = document.querySelector('#color');
+const size = document.querySelector('#size');
+const sliderContainer = document.querySelector('.slider-container');
+const slider = document.querySelector('#slider');
+const sliderValue = document.querySelector('.value');
+let defaultSize = 16;
+let grids = [];
+let colorValue = color.getAttribute('value');
 
-// default canvas
-createCanvas(16);
+// create default canvas
+createCanvas(defaultSize);
 
+// Change canvas size with slider
+slider.addEventListener('input', () => {
+    let value = slider.value;
+    sliderValue.textContent = `Square per side:${value}`;
+    gridContainer.textContent = '';
+    createCanvas(value);
+})
+
+
+// create canvas by given size
 function createCanvas(size) {
     for(i = 0; i < size*size; i++){
-        let grids = [];
         grids = document.createElement('div');
         gridContainer.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
         gridContainer.style.gridTemplateRows = `repeat(${size}, 1fr)`;
@@ -16,18 +32,39 @@ function createCanvas(size) {
     };
 }
 
-
+// Paint canvas red
 gridContainer.addEventListener('mouseover', e => {
     if(e.target.id !== 'grid-container'){
-        e.target.style.backgroundColor = 'red';
+        
+        e.target.style.backgroundColor = `${colorValue}`;
     }
 });
 
-const canvas = Array.from(document.querySelectorAll('.canvas'));
 
+
+
+
+// Toggle slider
+size.addEventListener('click', () => {
+    if(sliderContainer.style.visibility === 'hidden'){
+       sliderContainer.style.visibility = 'visible';
+    }
+    else{
+       sliderContainer.style.visibility = 'hidden';
+    }
+    
+});
+
+// Clear canvas to default color
 clear.addEventListener('click', () => {
-    canvas.forEach(e => {
+    gridContainer.childNodes.forEach(e => {
         e.style.backgroundColor = 'rgb(212, 212, 212)';
     });
     
+})
+
+// Color picker
+color.addEventListener('input', e => {
+    colorValue = e.target.value;
+    console.log(colorValue);
 })
