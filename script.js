0f4fba513e4ef7ff98c6ad5fdd2bf09212f405bd
crealyx <sanberk5416@hotmail.com>
2021-04-20 14:31:49 +0300

    Add color change feature on mouseOver event

--- script.js
+++ script.js
@@ -5,6 +5,12 @@ let grids = [];
 for(i = 0; i < 256; i++){
     grids = document.createElement('div');
     gridContainer.append(grids);
-}
+};
 
+gridContainer.addEventListener('mouseover', e => {
+    if(e.target.id !== 'grid-container'){
+        e.target.style.backgroundColor = 'red';
+    }
+    
+});
 
